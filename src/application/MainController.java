342d6f8a812a3fedbc2bea16a3fbf3586611edc6
Ghaff <mudashiruagm@gmail.com>
2019-05-23 17:18:19 +0000

    Added functionalities, play, pause etc

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -6,31 +6,85 @@ import java.util.ResourceBundle;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
-public class MainController implements Initializable{
+public class MainController{
 
 	@FXML private MediaView mv;
+	@FXML private Button vid;
+	
 	private MediaPlayer mp;
 	private Media me;
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		String path = new File("src/media/test.mp4").getAbsolutePath();
-		me = new Media(new File(path).toURI().toString());
-		mp = new MediaPlayer(me);
-		mv.setMediaPlayer(mp);
-		mp.setAutoPlay(true);
+	private File selectedFile;
+	
+	public void UploadVid(ActionEvent event)
+	{
+		FileChooser fc = new FileChooser();
 		
-		DoubleProperty width = mv.fitWidthProperty();
-		DoubleProperty height = mv.fitHeightProperty();
+		fc.getExtensionFilters().addAll(new ExtensionFilter("mp4", "*.mp4"));	
 		
-		width.bind(Bindings.selectDouble(mv.sceneProperty(), "width"));
-		height.bind(Bindings.selectDouble(mv.sceneProperty(), "height"));
+		selectedFile = fc.showOpenDialog(null);
+			
+		if (selectedFile != null)
+		{
+			String path = selectedFile.getAbsolutePath();
+			me = new Media(new File(path).toURI().toString());
+			
+			mp = new MediaPlayer(me);
+			
+			mv.setMediaPlayer(mp);
+			DoubleProperty width = mv.fitWidthProperty();
+			DoubleProperty height = mv.fitHeightProperty();
+			
+			width.bind(Bindings.selectDouble(mv.sceneProperty(), "width"));
+			height.bind(Bindings.selectDouble(mv.sceneProperty(), "height"));
+		}
 	}
 
+	public void play(ActionEvent event)
+	{
+		mp.play();
+		mp.setRate(1);
+	}
+	
+	public void pause(ActionEvent event)
+	{
+		mp.pause();
+	}
+	
+	public void fast(ActionEvent event)
+	{
+		mp.setRate(2);
+	}
+	
+	public void slow(ActionEvent event)
+	{
+		mp.setRate(0.5);
+	}	
+	
+	public void reload(ActionEvent event)
+	{
+		mp.seek(mp.getStartTime());
+		mp.play();
+	}
+	
+	public void start(ActionEvent event)
+	{
+		mp.seek(mp.getStartTime());
+		mp.stop();
+	}
+	
+	public void stop(ActionEvent event)
+	{
+		mp.seek(mp.getTotalDuration());
+		mp.stop();
+	}
 }
